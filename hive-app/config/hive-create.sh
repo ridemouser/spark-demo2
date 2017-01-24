bd3286f32bf1cbc0d93f76c974b6892a9079949a
ashish <kudvashish7@gmail.com>
2017-01-24 10:43:08 -0500

    committing all changes done before Jan 2017

---
+++ hive-app/config/hive-create.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+hadoop fs -mkdir /table
+hadoop fs -mkdir /table/account
+hadoop fs -put account.csv /table/account
+
+#/usr/local/hive/bin/schematool -dbType derby -initSchema &
+/usr/local/hive/bin/hive -e "create external table account (id int, acct_num string, acc_credit_limit float, acc_type string) row format delimited fields terminated by '|' stored as textfile location '/table/account'"
+/usr/local/hive/bin/hive -e "select * from account"
+
---
+++ scripts/apt-update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt-get update -Y
+apt-get install vim -Y
